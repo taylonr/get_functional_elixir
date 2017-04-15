c638f111f14ec8ee3c4229c40c885f3f9edfd25b
Nate Taylor <taylonr@gmail.com>
2017-04-14 21:14:08 -0500

    Writing stream count to compare against.

--- apps/get_functional_flow/lib/get_functional_flow.ex
+++ apps/get_functional_flow/lib/get_functional_flow.ex
@@ -3,10 +3,21 @@ defmodule GetFunctionalFlow do
   Documentation for GetFunctionalFlow.
   """
 
+  def benchmark(fun) do
+    fun
+    |> :timer.tc
+    |> elem(0)
+    |> Kernel./(1_000_000)
+  end
+
   @doc """
   Determines the number of words in a file
+
+    ### Example
+    iex> GetFunctionalFlow.benchmark(&GetFunctionalFlow.flow_count/0)
+
   """
-  def word_count do
+  def flow_count do
 
     File.stream!(Path.join(:code.priv_dir(:get_functional_flow), "kjv.txt"))
     |> Flow.from_enumerable()
@@ -17,6 +28,22 @@ defmodule GetFunctionalFlow do
       Map.update(acc, word, 1, & &1 + 1)
     end)
     |> Enum.to_list()
+    |> Enum.sort(fn first, second -> elem(first, 1) > elem(second, 1) end)
+  end
+
+  @doc """
+  Determines the number of words in a file
+
+    ### Example
+    iex> GetFunctionalFlow.benchmark(&GetFunctionalFlow.stream_count/0)
+  """
+  def stream_count do
+    File.stream!(Path.join(:code.priv_dir(:get_functional_flow), "kjv.txt"))
+    |> Stream.flat_map(&String.split(&1, [" ", "\n"], trim: true))
+    |> Stream.map(&String.downcase/1)
+    |> Enum.reduce(%{}, fn word, acc ->
+            Map.update(acc, word, 1, & &1 + 1)
+        end)
     |> Enum.sort(fn first, second -> elem(first, 1) > elem(second, 1) end)
   end
 end
